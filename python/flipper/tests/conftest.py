ad4e96daea3ccf8b6b81c78ea156e90d0888c5e8
Brian Cherinka <havok2063@hotmail.com>
2019-09-06 16:14:35 -0400

    adding to test to test new test url_prefix

--- python/flipper/tests/conftest.py
+++ python/flipper/tests/conftest.py
@@ -10,14 +10,31 @@
 
 from __future__ import print_function, division, absolute_import
 import pytest
-from flask import url_for
+from flask import url_for, request as freq
 from flipper.app import create_app
 
 
 @pytest.fixture
-def app():
+def app(monkeypatch, request):
+    ''' Flask application '''
     app = create_app()
     return app
+
+
+@pytest.yield_fixture
+def testctx(monkeypatch):
+    ''' Fixture to create an app with a test Flask base url
+
+    Returns only the request context to allow for use for url_for
+
+    '''
+    monkeypatch.setenv('FLIPPER_BASE', 'test/flipper')
+    app = create_app()
+    app.testing = True
+    ctx = app.test_request_context()
+    ctx.push()
+    yield
+    ctx.pop()
 
 
 def test_app(client):
@@ -27,3 +44,20 @@ def test_app(client):
 def test_ok_status(client):
     res = client.get(url_for('index.status'))
     assert res.json == {'flipper status': 'ok'}
+
+
+def _get_urls(base):
+    home = url_for('index.home')
+    static = url_for('static', filename='sdss-logo.png')
+    assert home == '{0}'.format(base)
+    assert static == '{0}static/sdss-logo.png'.format(base)
+
+
+def test_urls(client):
+    ''' test of basic flipper urls '''
+    _get_urls('/flipper/')
+
+
+def test_testbase(testctx):
+    ''' test of flipper test server urls '''
+    _get_urls('/test/flipper/')
