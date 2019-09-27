cabfdb89d65ea0967a7938dc76b587043102a11e
Brian Cherinka <havok2063@hotmail.com>
2019-09-27 15:15:34 -0400

    updating flipper to allow for flexible releases and prod/test envs

--- python/flipper/controllers/index.py
+++ python/flipper/controllers/index.py
@@ -9,10 +9,14 @@
 # @Last Modified time: 2018-08-16 11:58:34
 
 from __future__ import print_function, division, absolute_import
+import os
 import flipper.app
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 
 index = Blueprint("index", __name__)
+
+# SDSS DR releases
+releases = ['dr15', 'dr16']
 
 
 @index.route('/status/', methods=['GET', 'POST'], endpoint='status')
@@ -23,5 +27,29 @@ def status():
 @index.route('/', methods=['GET'], endpoint='home')
 @index.route('/index/', methods=['GET'], endpoint='index')
 def home():
-    output = {'title': 'SDSS Splashpage', 'version': flipper.app.__version__}
+    # get release variable from Flask request
+    release = request.environ.get('FLIPPER_RELEASE', None)
+
+    # if no release try the os environment variable
+    if not release:
+        release = os.environ.get("FLIPPER_RELEASE", None)
+
+    # if no release, select the lastest one
+    if not release:
+        sorted_rels = sorted(releases, key=lambda x: int(x.split('dr')[-1]))
+        release = sorted_rels[-1]
+
+    # set base and marvin urls for production or testing
+    istestenv = 'test' in request.path
+    if istestenv:
+        base_url = '{0}.sdss.utah.edu'.format(release)
+        marvin_url = 'lore.sdss.utah.edu/test'
+    else:
+        base_url = '{0}.sdss.org'.format(release)
+        marvin_url = base_url
+    print(release, base_url, marvin_url)
+    # build template parameter dictionary
+    output = {'title': 'SDSS Splashpage', 'version': flipper.app.__version__,
+              'release': release, 'istest': istestenv, 'base_url': base_url,
+              'marvin_url': marvin_url}
     return render_template('index.html', **output)
